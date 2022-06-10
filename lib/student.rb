4f9ea65fb500602d4f275c0bda1d39dfde4fc00f
omar3o3 <otantawy53@gmail.com>
2022-06-10 15:31:02 -0400

    commit tree 0db400316e364cc986160bd4eaf3313934f79b94

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative "./user.rb"
+require 'pry'
+
+class Student < User
+
+    attr_reader :knowledge    
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(input)
+        @knowledge << input
+    end
 
 end
